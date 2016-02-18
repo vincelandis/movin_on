0b1a219c5eff32f01b0116321b3bb56f9c91e2ce
Vince Landis <vincelandis@gmail.com>
2016-02-18 12:52:42 -0500

    First commit with initial project files
    First commit, added initial iOS project files

---
+++ movin_on/FirstViewController.m
@@ -0,0 +1,27 @@
+//
+//  FirstViewController.m
+//  movin_on
+//
+//  Created by Vince Landis on 2/18/16.
+//  Copyright © 2016 vincelandis. All rights reserved.
+//
+
+#import "FirstViewController.h"
+
+@interface FirstViewController ()
+
+@end
+
+@implementation FirstViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
